8d49fa55b95504f14a791bff79f62d0cd3bcaf84
Nathan Ngobale <nngobale@DVTL101621.local>
2020-02-25 13:30:12 +0200

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/NateLib/NateLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NateLib : NSObject
+@end
+@implementation PodsDummy_NateLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-NateLib_Tests/Pods-NateLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NateLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NateLib_Tests
+@end
